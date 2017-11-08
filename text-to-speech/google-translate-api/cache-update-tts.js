0b671958333f2e6b5c4b5fb58b9a7079631c0aad
clemeno <clemeno@gmail.com>
2017-11-08 23:55:26 +0100

    comment critical parts + sanitize text for filenameText

--- text-to-speech/google-translate-api/cache-update-tts.js
+++ text-to-speech/google-translate-api/cache-update-tts.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
-// node native
+// usage:
+// node cache-update-tts.js preset "text"
+// will put the result in ../sound-bank/preset/filenameText.mp3
+// es/node natives
 /* globals require, process, Promise, console */
+// import node natives
 const https = require( 'https' ), fs = require( 'fs' );
 ( () => {
   if ( 2 < process.argv.length ) {
-    const preset = ( {
-      'fr-fr': { in: 'fr-fr', out: 'fr-fr' },
-      'fr-jp': { in: 'fr-fr', out: 'ja-jp' }
-    } )[ process.argv[ 2 ] ],
-      text = encodeURIComponent( process.argv[ 3 ] );
-    // get remote file content, save to local file
+    const _argv2 = '' + ( process.argv[ 2 ] || '' ),
+      _argv3 = '' + ( process.argv[ 3 ] || '' ),
+      // prepare the text to query
+      text = encodeURIComponent( _argv3 ),
+      // forbidden characters in filename for windows 10
+      unsafeFilenameChars = [ '<', '>', ':', '"', '/', '\\', '|', '?', '*' ],
+      // sanitize input text for filenameText
+      filenameText = _argv3.split( '' ).map( char => -1 < unsafeFilenameChars.indexOf( char ) ? '_' : char ).join( '' ),
+      // use presets to reduce to one locale argument
+      presets = { 'fr-fr': { in: 'fr-fr', out: 'fr-fr' }, 'fr-jp': { in: 'fr-fr', out: 'ja-jp' } },
+      // select the requested preset
+      preset = Object.assign( {}, presets[ _argv2 ] );
+    // 1/2 get remote file content, 2/2 save to local file
     function grab( _ = { get: '', save: '' } ) {
       if ( _.get && _.save ) {
         const file = fs.createWriteStream( _.save );
@@ -18,8 +29,8 @@ const https = require( 'https' ), fs = require( 'fs' );
           let bResponseSent = false;
           https.
             get( _.get, res => {
-              // TODO: preserve existing file until 'finish'
-              // TODO: remove unfinished temp files
+              // TODO: 1/2 preserve existing file until 'finish'
+              // TODO: 2/2 remove unfinished temp files
               res.pipe( file );
               file.on( 'finish', () => file.close( () => {
                 if ( !bResponseSent ) {
@@ -39,7 +50,7 @@ const https = require( 'https' ), fs = require( 'fs' );
     }
     grab( {
       get: `https://translate.google.com/translate_tts?ie=UTF-8&tl=${preset.out}&client=${preset.in}&q=${text}`,
-      save: `../sound-bank/${preset.in.split( '-' )[ 0 ]}-${preset.out.split( '-' )[ 1 ]}/${text}.mp3`
+      save: `../sound-bank/${preset.in.split( '-' )[ 0 ]}-${preset.out.split( '-' )[ 1 ]}/${filenameText}.mp3`
     } ).
       then( res => console.log( 'File updated' ) ).
       catch( e => console.error( e ) );
